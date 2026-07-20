182ca1dcdc3bdbb819ca52062e7d9cf904f8fd31
Shubham Mahalle <shubhammahallee@gmail.com>
2026-07-20 15:17:34 +0530

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,7 +1,5 @@
 import pytest
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+import undetected_chromedriver as uc
 from Utilities.readConfig import ReadConfig
 
 def pytest_addoption(parser):
@@ -11,17 +9,13 @@ def pytest_addoption(parser):
 def setup(request):
     browser = request.config.getoption("--browser")
     
-    options = webdriver.ChromeOptions()
+    options = uc.ChromeOptions()
     options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--disable-gpu")
     options.add_argument("--window-size=1920,1080")
     
-    driver = webdriver.Chrome(
-        service=Service(ChromeDriverManager().install()),
-        options=options
-    )
+    driver = uc.Chrome(options=options, version_main=None)
     
     driver.maximize_window()
     driver.get(ReadConfig.get_baseurl())
